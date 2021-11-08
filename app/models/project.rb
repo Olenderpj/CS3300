5914f591bde9faa1daa2bae3f13b0ac319c256be
PJ <olenderpj1@gmail.com>
2021-11-08 18:40:19 +0000

    HW6 - Test to ensure that validation of title and description are working again

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
     #validate the existence of both the title and descriptions
-    #validate_presence_of :title, :description
+    validate_presence_of :title, :description
 end
